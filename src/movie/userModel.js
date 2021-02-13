8950aaab770f264db5877dd6120557d7dcab8649
FloBlum <33659187+FloBlum@users.noreply.github.com>
2021-02-13 14:02:16 +0100

    Implemented password hashing

--- src/movie/userModel.js
+++ src/movie/userModel.js
@@ -1,17 +1,26 @@
+import * as argon from "argon2";
+
 export async function getByUsername(db, username) {
   return await db.get("SELECT * FROM users WHERE username= ?", username);
 }
 
 export async function passwordIsCorrect(user, password) {
-  if (user && password) {
-    return user.password === password;
-  } else {
-    return false;
-  }
+  const hashedPassword = user.password;
+  const correctPassword = await validatePassword(hashedPassword, password);
+  return correctPassword;
 }
 
 export async function editUser(db, username, data) {
   const result = await db.run("UPDATE users SET first_Name=?, last_Name=?, favorites=? WHERE username = ?", 
   data.first_name, data.last_name, data.favorites, username);
   return result.changes;
+}
+
+
+async function validatePassword(hashedPassword, plainPassword) {
+  return await argon.verify(hashedPassword, plainPassword);
+}
+
+async function hashPassword(password) {
+  return await argon.hash(password);
 }
